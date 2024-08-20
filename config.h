508429e715496e74916096ed6eac9e1583927d71
aaronshahriari <aaronshahriari@gmail.com>
2024-08-20 05:28:55 -0400

    update dmenu; need to update colors

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"CaskaydiaCoveNerdFontMono:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -17,8 +17,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 8;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -27,4 +27,4 @@ static unsigned int columns    = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;
