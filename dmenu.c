8f9df80f2accc8872f880813182439ff097d844e
Aaron Shahriari <aaronshahriari@gmail.com>
2025-01-14 06:34:05 -0500

    reverted to old width thing

--- dmenu.c
+++ dmenu.c
@@ -98,10 +98,10 @@ calcoffsets(void)
 static int
 max_textw(void)
 {
-    int len = 0;
-    for (struct item *item = items; item && item->text; item++)
-        len = MAX(TEXTW(item->text) * (columns == 0 ? 1 : columns), len);
-    return len;
+	int len = 0;
+	for (struct item *item = items; item && item->text; item++)
+		len = MAX(TEXTW(item->text), len);
+	return len;
 }
 
 static void
